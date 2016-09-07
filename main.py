a7bafad20697e33716da5210a67997178b9645b6
Guilherme Diego <guidiego.expgames@gmail.com>
2016-09-07 17:37:01 -0300

    Dockerfile and Main

---
+++ main.py
@@ -0,0 +1,19 @@
+import json
+
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+users = []
+
+@app.route('/user', methods=['GET'])
+def list_users():
+    return jsonify(users)
+
+@app.route('/user', methods=['POST'])
+def create_user():
+    user = json.loads(request.data.decode("utf-8"))
+    users.append(user)
+    return jsonify(user)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
